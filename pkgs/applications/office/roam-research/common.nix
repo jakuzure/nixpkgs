83cd042660a06256d4bdab2c6048e0078fb88c14
Christopher Rzipa <christopher.rzipa@whizus.com>
2023-10-01 11:02:34 +0200

    roam-research: sha256 -> hash

--- pkgs/applications/office/roam-research/common.nix
+++ pkgs/applications/office/roam-research/common.nix
@@ -7,16 +7,16 @@ in
   inherit pname version;
   sources = {
     x86_64-darwin = fetchurl {
-      sha256 = "sha256-jyFNH3qrgrsftExL/b2t8bY3W3fYVz+Gp11AuaIMxbg=";
       url = "https://roam-electron-deploy.s3.us-east-2.amazonaws.com/Roam+Research-${version}.dmg";
+      hash = "sha256-jyFNH3qrgrsftExL/b2t8bY3W3fYVz+Gp11AuaIMxbg=";
     };
     aarch64-darwin = fetchurl {
-      sha256 = "sha256-AnyvFCbyUi6tcgxYQAj+zPLl4/kVh9ZeupetRhzH0PU=";
       url = "https://roam-electron-deploy.s3.us-east-2.amazonaws.com/Roam+Research-${version}-arm64.dmg";
+      hash = "sha256-AnyvFCbyUi6tcgxYQAj+zPLl4/kVh9ZeupetRhzH0PU=";
     };
     x86_64-linux = fetchurl {
-      sha256 = "sha256-veDWBFZbODsdaO1UdfuC4w6oGCkeVBe+fqKn5XVHKDQ=";
       url = "https://roam-electron-deploy.s3.us-east-2.amazonaws.com/${pname}_${version}_amd64.deb";
+      hash = "sha256-veDWBFZbODsdaO1UdfuC4w6oGCkeVBe+fqKn5XVHKDQ=";
     };
   };
 }
